e37389d24bf017db7519063b0ad0032cc424499f
Vu Dinh <vuniverse143@gmail.com>
2022-03-26 01:16:41 -0700

    experimental design of rear card

--- lib/models/linkskill_model.dart
+++ lib/models/linkskill_model.dart
@@ -1,98 +1,31 @@
-import 'package:legionprovider/models/linkskill_model.dart';
 import 'package:flutter/material.dart';
 
-class Character extends StatelessWidget {
-  final String name;
-  final String? shortName;
-  final int level;
-  final String bonusStat;
-  final List<int> bonusValues;
-  final String bonusUnit;
-  late final String imgurl;
+class LinkSkill extends StatelessWidget {
+  final String name = "Elementalism";
+  final String job = "Kanna";
+  final String desc =
+      "Deepens your connection to the elements to provide a permanent damage increase.";
+  final String bonusStat = "Damage";
+  final List<int> bonusValues = [5, 10];
+  final String bonusUnit = "%";
+  final String imgurl = "./img/Elementalism.webp";
   late final int bonusValue;
-  late final LinkSkill link;
 
-  Character({
+  LinkSkill({
     Key? key,
-    required this.name,
-    required this.shortName,
-    required this.level,
-    required this.bonusStat,
-    required this.bonusValues,
-    required this.bonusUnit,
+    // required this.name,
+    // required this.job,
+    // required this.desc,
+    // required this.bonusValues,
   }) : super(key: key) {
     bonusValue = calculateBonusValue();
-    imgurl = parseImgURL();
   }
-
-  factory Character.copy(Character other) {
-    return Character(
-      name: other.name,
-      shortName: other.shortName,
-      level: other.level,
-      bonusStat: other.bonusStat,
-      bonusValues: other.bonusValues,
-      bonusUnit: other.bonusUnit,
-    );
-  }
-
-  factory Character.fromMap(Map<String, dynamic> data) {
-    final name = data['name'] as String;
-    final shortName = data['shortName'] as String?;
-    final level = data['level'] as int;
-    final bonusStat = data['bonusStat'] as String;
-    final bonus = data['bonusValues'] as List<dynamic>;
-    final bonusValues = bonus.map((n) => n as int).toList();
-    final bonusUnit = data['unit'] as String;
-    return Character(
-      name: name,
-      shortName: shortName,
-      level: level,
-      bonusStat: bonusStat,
-      bonusValues: bonusValues,
-      bonusUnit: bonusUnit,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      if (shortName != null) 'shortName': shortName,
-      'level': level,
-      'bonusStat': bonusStat,
-      'bonusValues': bonusValues,
-      'unit': bonusUnit,
-    };
-  }
-
   int calculateBonusValue() {
-    int value = 0;
-    if (level >= 250) {
-      value = bonusValues[4];
-    } else if (level >= 200) {
-      value = bonusValues[3];
-    } else if (level >= 140) {
-      value = bonusValues[2];
-    } else if (level >= 100) {
-      value = bonusValues[1];
-    } else if (level >= 60) {
-      value = bonusValues[0];
-    }
-    return value;
-  }
-
-  String parseImgURL() {
-    String url = "./img/" + name.replaceAll(' ', '') + ".webp";
-    return url;
+    return bonusValues[1];
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildRear(context);
-  }
-
-  Widget _buildFront(BuildContext context) {
-    final String cardTitle = (shortName != null) ? shortName.toString() : name;
     return Container(
       key: const ValueKey(true),
       decoration: const BoxDecoration(
@@ -122,7 +55,7 @@ class Character extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        cardTitle,
+                        name,
                         style: Theme.of(context)
                             .textTheme
                             .headlineMedium!
@@ -133,7 +66,7 @@ class Character extends StatelessWidget {
                         overflow: TextOverflow.ellipsis,
                       ),
                       Text(
-                        "Level " + level.toString(),
+                        "Job: " + job,
                         style: Theme.of(context).textTheme.subtitle1!.copyWith(
                               color: Colors.grey.shade300,
                               decoration: TextDecoration.underline,
@@ -179,9 +112,5 @@ class Character extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Widget _buildRear(BuildContext context) {
-    return LinkSkill();
   }
 }
